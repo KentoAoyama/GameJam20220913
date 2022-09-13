714512e6d8a6aeea89bb6496af08e69cc5a8d47c
sinzitoyama <yamat422@gmail.com>
2022-09-13 17:22:59 +0900

    音追加

--- Assets/Toyama/PlayerBullet.cs
+++ Assets/Toyama/PlayerBullet.cs
@@ -6,7 +6,9 @@ public class PlayerBullet : MonoBehaviour
 {
     [SerializeField] float _bulletSpeed = 10;
     [SerializeField] GameObject _hitEffect;
-
+    [SerializeField] AudioSource _enemyBullete;
+    [SerializeField] AudioSource _enemy;
+    [SerializeField] AudioSource _boss;
     Rigidbody2D _rb;
 
 
@@ -19,7 +21,19 @@ public class PlayerBullet : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D collision)
     {
+        if(collision.gameObject.tag == "EnemyBullet")
+        {
+            _enemyBullete.Play();
+        }
+        else if(collision.gameObject.tag == "Boss")
+        {
+            _boss.Play();
+        }
+        else if(collision.gameObject.tag == "Enemy")
+        {
+            _enemy.Play();
+        }
         Instantiate(_hitEffect, transform.position, transform.rotation);
-        Destroy(gameObject);
+        Destroy(gameObject,0.2f);
     }
 }
